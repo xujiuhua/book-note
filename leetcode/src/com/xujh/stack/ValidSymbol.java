ec45fec7dad01042ccf4f264a964cc5b03c28f81
jiuhua.xu <xujiuhuamoney@163.com>
2019-06-26 23:31:58 +0800

    :sparkles: Adding valid symbol

---
+++ leetcode/src/com/xujh/stack/ValidSymbol.java
@@ -0,0 +1,49 @@
+package com.xujh.stack;
+
+import java.util.Stack;
+
+/**
+ * <p>
+ * 有效的括号
+ * https://leetcode-cn.com/explore/learn/card/queue-stack/218/stack-last-in-first-out-data-structure/878/
+ * </p>
+ *
+ * @author jiuhua.xu
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class ValidSymbol {
+
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                Character pop = stack.pop();
+                if (c == ')' && pop != '(') {
+                    return false;
+                }
+                if (c == ']' && pop != '[') {
+                    return false;
+                }
+                if (c == '}' && pop != '{') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidSymbol symbol = new ValidSymbol();
+        boolean valid = symbol.isValid("{[()]}");
+        System.out.println(valid);
+        boolean valid2 = symbol.isValid("{[(})]}");
+        System.out.println(valid2);
+    }
+}
