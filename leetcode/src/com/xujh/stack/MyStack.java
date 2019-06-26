3c28bd03dfec2988e58b02e2074cf48696b6da01
jiuhua.xu <xujiuhuamoney@163.com>
2019-06-26 15:52:15 +0800

    :sparkles: Adding my stack

---
+++ leetcode/src/com/xujh/stack/MyStack.java
@@ -0,0 +1,52 @@
+package com.xujh.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p></p>
+ *
+ * @author jiuhua.xu
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class MyStack {
+    private List<Integer> data;
+
+    public MyStack() {
+        this.data = new ArrayList<>();
+    }
+
+    public void push(int x) {
+        data.add(x);
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public int top() {
+        return data.get(data.size() - 1);
+    }
+
+    public boolean pop() {
+        if (isEmpty()) {
+            return false;
+        }
+        data.remove(data.size() - 1);
+        return true;
+    }
+
+    public static void main(String[] args) {
+        MyStack s = new MyStack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        for (int i = 0; i < 4; ++i) {
+            if (!s.isEmpty()) {
+                System.out.println(s.top());
+            }
+            System.out.println(s.pop());
+        }
+    }
+}
